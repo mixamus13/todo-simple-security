3ba95c24caa34784096eec5a53c8ad14c577c8cb
maxim13 <ZLf7XV6s13>
2021-07-03 19:23:03 +0300

    Initial commit

---
+++ src/main/java/com/apress/todo/domain/ToDo.java
@@ -0,0 +1,59 @@
+package com.apress.todo.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+public class ToDo {
+
+  @Id
+  @GeneratedValue(generator = "system_uuid")
+  @GenericGenerator(name = "system_uuid", strategy = "uuid")
+  private String id;
+
+  @NotNull
+  @NotBlank
+  private String description;
+
+  @Column(insertable = true, updatable = false)
+  private LocalDateTime created;
+
+  private LocalDateTime modified;
+
+  private boolean completed;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public ToDo() { }
+
+  public ToDo(String description) {
+    this.description = description;
+  }
+
+  @PrePersist
+  void onCreate() {
+    this.setCreated(LocalDateTime.now());
+    this.setModified(LocalDateTime.now());
+  }
+
+  @PreUpdate
+  void onUpdate() {
+    this.setModified(LocalDateTime.now());
+  }
+}
